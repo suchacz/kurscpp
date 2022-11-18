fd41213738f4713e5218c133f305c4eb5cb4ec10
suchacz <szymonuchacz@gmail.com>
2022-11-18 16:32:42 +0100

    poprawka

--- dzien_02/01_wiersz.cpp
+++ dzien_02/01_wiersz.cpp
@@ -4,9 +4,9 @@ int main () {
     int N = 0;
     std::cin >> N;
     int liczba_znakow;
-    while   (liczba_znakow < N - 1) {
+    while   (liczba_znakow < N) {
         std::cout << "#";
         liczba_znakow += 1;
     }
-    std::cout << "#\n";
+    std::cout << "\n";
 }
