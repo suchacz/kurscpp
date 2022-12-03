f96b4af860adad044ad90a461c20d42225a44dc8
suchacz <szymonuchacz@gmail.com>
2022-12-03 10:28:35 +0100

    wersja z podwojna tablica

--- dzien_05/00_rozgrzewka.cpp
+++ dzien_05/00_rozgrzewka.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-bool pole_ok(in x, int y) {
+bool pole_ok(int x, int y) {
     return x >= 0 && x <=7 && y >= 0 && y <= 7;
 }
 
@@ -8,10 +8,11 @@ bool pole_ok(in x, int y) {
 
 int ile_ruchow(int x, int y) {
     int suma = 0;
-    int wspx[8] = {2, 2, -2, -2, 1, 1, -1, -1};
-    int wspy[8] = {1, -1, 1, -1, 2, -2, 2, -2};
+    const int wsp[8][2] = {{2, 1}, {2, -1}, {-2, 1}, {-2, 1},
+                            {1, 2}, {1, -2}, {-1, 2}, {-1, 2} 
+    };
     for (int i = 0; i < 8; i += 1) {
-    if (pole_ok(x + wspx[i], y + wspy[i]))
+        if (pole_ok(x + wsp[i][0], y + wsp[i][1]))
         suma +=1 ;
     }
     
@@ -21,6 +22,6 @@ int ile_ruchow(int x, int y) {
 int main () {
     int x;
     int y;
-    std::cin >> x >> y:
+    std::cin >> x >> y;
     std::cout << "Mozliwe ruchy:" << ile_ruchow(x, y) << "\n";
 }
